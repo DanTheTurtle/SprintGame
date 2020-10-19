397f44f4bf1a4870395e5a41ea19fcf7ad2b9934
DanTheTurtle <djd925@gmail.com>
2020-10-19 16:27:42 -0500

    restructured repo. Character can move and has light, is not yet able to die

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//? Dan comments
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundMask;
+    private Rigidbody2D rb;
+    private CircleCollider2D cc; //? maybe keep as circle, should not matter much
+    private float speed = 7f;
+    private float jumpVelo = 17f;
+    private float jumpForce = 500f;
+    private Boolean hasRejump = true; //?
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cc = GetComponent<CircleCollider2D>();
+    }
+
+    void Update()
+    {
+        float horzIn = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(horzIn * speed * Time.deltaTime, 0, 0);
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpVelo;
+        }
+        else if (hasRejump && Input.GetKeyDown(KeyCode.Space))
+        {
+            hasRejump = false;
+            rb.velocity = Vector2.up * jumpVelo;
+        }
+        //? relic of old game
+        /*else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.velocity = (Vector2.up * jumpVelo * -1);
+        }*/
+
+        if (IsGrounded())
+        {
+            refreshJump();
+        }
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collider)
+
+    public void refreshJump() { hasRejump = true; }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.CircleCast(cc.bounds.center, cc.radius, Vector2.down, .01f, groundMask);
+        //Debug.Log(raycastHit2D.collider);
+        return raycastHit2D.collider != null;
+    }
+}
